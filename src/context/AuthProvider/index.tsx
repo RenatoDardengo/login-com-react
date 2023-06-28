1111369948c338dd9d50a69b3745a280294c83f3
Renato Dardengo <rdardengo01@gmail.com>
2023-06-27 22:07:32 -0300

    Estruturando index.tsx

--- src/context/AuthProvider/index.tsx
+++ src/context/AuthProvider/index.tsx
@@ -1,6 +1,24 @@
 //é o arquivo onde o contexto será criado
-import React,{Context, createContext} from "react";
-import { IContext } from "./types";
+import React,{Children, Context, createContext, useState} from "react";
+import { IAuthProvider, IContext, IUser } from "./types";
 
 export const AuthContext = createContext<IContext>({} as IContext);
 
+//o AuthProvider é quem ficará por fora da aplicação verificando quem fez login ou logout
+export const AuthProvider=({children}:IAuthProvider)=>{
+    //ela precisa ter um estado interno, pois é ele quem vai disponibilizar informações para a aplicação
+    const [user, setUser]= useState<IUser | null>();
+
+    async function authenticate(email:string, password:string) {
+        
+    }
+
+    function logout(){
+
+    }
+    return(
+        <AuthContext.Provider value={{...user, authenticate, logout}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
